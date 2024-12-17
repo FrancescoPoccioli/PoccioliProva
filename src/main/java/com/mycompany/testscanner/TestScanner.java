89dd0bc780e60d330a828d4ac94a06856abed8a5
Hiper236 <jakuballababidi@gmail.com>
2024-12-17 13:22:41 +0100

    Primo commit nel ramo main

---
+++ src/main/java/com/mycompany/testscanner/TestScanner.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.testscanner;
+
+/**
+ *
+ * @author B045_08
+ */
+public class TestScanner {
+    
+    
+  
+    public static void main(String[] args) throws IOException {
+
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("xanadu.txt")));
+
+            while (s.hasNext()) {
+                System.out.println(s.next());
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+    }
+}
+
+   
